4591de76cc6ef30392a5ce82f7c13e9a0d252b77
Alexander2817 <alande10@charlotte.edu>
2025-03-22 20:37:41 -0400

    feat:Created resources.py

--- api/models/schemas.py
+++ api/models/schemas.py
@@ -2,26 +2,7 @@ from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel
 
-
-class SandwichBase(BaseModel):
-    sandwich_name: str
-    price: float
-
-
-class SandwichCreate(SandwichBase):
-    pass
-
-
-class SandwichUpdate(BaseModel):
-    sandwich_name: Optional[str] = None
-    price: Optional[float] = None
-
-
-class Sandwich(SandwichBase):
-    id: int
-
-    class ConfigDict:
-        from_attributes = True
+from api.models.models import Sandwich
 
 
 class ResourceBase(BaseModel):
